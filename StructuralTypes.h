ab990528afabe0a81c5c5d57c469d10062f742c3
SysMC <mbee@sysmc.ru>
2017-06-20 15:55:00 +0300

    Add files via upload

---
+++ StructuralTypes.h
@@ -0,0 +1,30 @@
+typedef struct  //Объявляем структурный тип nodeAddress
+{
+  byte msb;
+  byte lsb;
+} moduleAddress_t;
+
+typedef struct  //Объявляем структурный тип frameHeader
+{
+  byte sof;
+  byte lenMsb;
+  byte lenLsb;
+} frameHeader_t;
+
+typedef struct  //Объявляем структурный тип keyPacket
+{
+  frameHeader_t frameHeader;
+  byte frameType;
+  moduleAddress_t destination;
+  byte receivedSSI;
+  byte options;
+  byte text[10];
+  byte checkSum;             //Контрольная сумма
+} testPacket_t;
+
+typedef enum
+{
+  WAIT_FOR_ANSWER,
+  PAUSE
+} fsm_t;
+
